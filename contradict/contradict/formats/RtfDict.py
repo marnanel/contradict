87b7695c9c7e8e161f2eb00e80facc79f53cc2e2
Thomas Thurman <thomas@thurman.org.uk>
2017-03-07 20:19:27 +0000

    merge in RTF writer

--- contradict/contradict/formats/RtfDict.py
+++ contradict/contradict/formats/RtfDict.py
@@ -19,12 +19,31 @@ class RtfDict(StenoDict):
 
 	@classmethod
 	def load_from_file(cls, filename):
-		pass
+		raise ValueError("rtf loading support is not yet implemented")
 
 	@classmethod
 	def can_save_to_file(cls):
 		return True
 
 	@classmethod
-	def save_to_file(cls, filename):
-		pass
+	def _rtf_escape(s):
+		for c in r'\{}':
+			s = s.replace(c, '\\'+c)
+		return s
+
+	@classmethod
+	def save_to_file(cls, filename, contents):
+		fh = open(filename, 'wb')
+
+		fh.write(r'{\rtf1\ansi{\*\cxrev100}'+
+			r'\cxdict{\*\cxsystem contradict.marnanel.org}'+
+			r'{\stylesheet{\s0 Normal;}}'+
+			'\r\n')
+
+		for (key, value) in contents.enumerate():
+			fh.write(r'{\*\cxs %(steno)s}%(translation)s\r\n' % {
+				'key': key,
+				'value': value,
+				})
+
+		fh.write(r'}\r\n')
