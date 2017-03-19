1a4cef43bb79be443fb5b3d45ec22569e9e1882f
Thomas Thurman <thomas@thurman.org.uk>
2017-03-19 17:18:03 +0000

    files loaded in blocks; start of breaking out data

--- mdb/jet_dict.py
+++ mdb/jet_dict.py
@@ -2,15 +2,24 @@
 
 import struct
 
-TABLE_CONTROL_PAGE_TYPE = chr(2)
+DATA_PAGE_TYPE = 1
+TABLE_CONTROL_PAGE_TYPE = 2
 
 PAGE_SIZE = 4096
 REALINDEX_ENTRY_SIZE = 12
 COLUMN_ENTRY_SIZE = 25
 
+# Pages of all types:
+PAGE_TYPE_OFFSET = 0
+
+# Table control pages:
 COLUMN_COUNT_OFFSET = 45
 REALINDEX_COUNT_OFFSET = 51
 COLUMNS_START_OFFSET = 63
+
+# Data pages:
+CONTROLLING_TABLE_OFFSET = 4
+ROW_COUNT_OFFSET = 12
 
 SIZE_TO_FORMAT = {
 	1: "<B",
@@ -24,8 +33,10 @@ class JetDictionary(object):
 	def __init__(self, filename):
 		self._fh = open(filename, 'rb')
 
-		self._find_control()
-		print 'Control:', self._control_page, self._dictionary_fields
+		self._control = self._find_control()
+		print 'Control:', self._control
+
+		self._find_data()
 
 	def _find_control(self):
 		# First we need to find the control page for the Dictionary table.
@@ -36,17 +47,17 @@ class JetDictionary(object):
 		# just spin through and look for that.
 
 		# The earliest possible index of the dictionary table is 3.
-		self._control_page = 3
+		page = 3
 
 		while True:
-			self._fh.seek(self._control_page * PAGE_SIZE)
-			page_type = self._fh.read(1)
-			if not page_type:
+
+			self._load_page(page)
+			if not self._page:
 				# hit the end of the file; give up
 				raise ValueError("no dictionary found")
 
+			page_type = self.get_int(PAGE_TYPE_OFFSET, 1)
 			if page_type==TABLE_CONTROL_PAGE_TYPE:
-				print '----', self._control_page
 
 				column_count = self.get_int(COLUMN_COUNT_OFFSET, size=2)
 				index_count = self.get_int(REALINDEX_COUNT_OFFSET, size=4)
@@ -80,23 +91,46 @@ class JetDictionary(object):
 
 				if len(result)==len(USEFUL_COLUMNS):
 					# we've caught them all!
-					self._dictionary_fields = result
-					return
+					result['page'] = page
+					return result
 
-			self._control_page += 1
+			page += 1
+
+	def _find_data(self):
+
+		# I suspect that data pages can't occur before the
+		# corresponding control page, but I don't know
+		# this for sure, so let's be on the safe side
+		# and start reading from the beginning.
+		page = 3
+
+		while True:
+			self._load_page(page)
+			if not self._page:
+				# hit the end of the file; we're done
+				return
+
+			page_type = self.get_int(PAGE_TYPE_OFFSET, 1)
+			if page_type==DATA_PAGE_TYPE:
+
+				controlling_table = self.get_int(CONTROLLING_TABLE_OFFSET, 4)
+				if controlling_table==self._control['page']:
+					row_count = self.get_int(ROW_COUNT_OFFSET, 2)
+					print row_count
+
+			page += 1
 
 	def get_int(self, offset=0, size=2):
-
-		self._fh.seek(self._control_page*PAGE_SIZE + offset)
-		encoded = self._fh.read(size)
-
+		encoded = self._page[offset:offset+size]
 		return struct.unpack(SIZE_TO_FORMAT[size], encoded)[0]
 
 	def get_string(self, offset=0, length=0):
-
-		self._fh.seek(self._control_page*PAGE_SIZE + offset)
-		encoded = self._fh.read(length)
+		encoded = self._page[offset:offset+length]
 		return bytes.decode(encoded, encoding='UTF-16')
+
+	def _load_page(self, page_number):
+		self._fh.seek(page_number*PAGE_SIZE)
+		self._page = self._fh.read(PAGE_SIZE)
 
 if __name__=='__main__':
 	print JetDictionary('/tmp/stened.dct')
